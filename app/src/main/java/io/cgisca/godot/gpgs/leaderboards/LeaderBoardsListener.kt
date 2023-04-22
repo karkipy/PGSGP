d4f2253a44697c649e5b0f3a32d751bcfb9bfc5c
karkipy <aashish.y2z@gmail.com>
2023-04-22 18:47:07 +0545

    add function to retrive leader board and update listener

--- app/src/main/java/io/cgisca/godot/gpgs/leaderboards/LeaderBoardsListener.kt
+++ app/src/main/java/io/cgisca/godot/gpgs/leaderboards/LeaderBoardsListener.kt
@@ -3,4 +3,6 @@ package io.cgisca.godot.gpgs.leaderboards
 interface LeaderBoardsListener {
     fun onLeaderBoardScoreSubmitted(leaderboardId: String)
     fun onLeaderBoardScoreSubmittingFailed(leaderboardId: String)
+    fun onCurrentPlayerLeaderBoardScoreLoadingFailed(leaderboardId: String)
+    fun onCurrentPlayerLeaderBoardScoreLoaded(leaderboardId: String, scoreJson: String)
 }
